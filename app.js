2773d859ea9e77496b87deb52d0e19215f65f717
IshikaSingla <ishikasingla0823@gmail.com>
2020-06-16 13:55:06 +0530

    fix

--- app.js
+++ app.js
@@ -2,17 +2,13 @@ var express = require('express')
 var bodyParser = require('body-parser')
 
 var app = express()
-app.set('view engine', 'ejs')
 
-var indexRoute = require('./index')
-
-app.use(express.static(__dirname + '/public/images'))
+app.use(express.static(__dirname + '/public'))
+app.get("/", function(req,res){
+    res.render("index");
+})
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// ==================================ROUTES============================================================
-// localhost:3000/
-app.listen(process.env.PORT || 3500, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('Server is running....')
 })
-
-app.use(indexRoute)
